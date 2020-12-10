5dd3f0936aac28ce87f2e190122f2a3158fca743
lixianjun02 <lixianjun02@meituan.com>
2020-12-10 23:47:14 +0800

    ClassLoaderUtils

--- src/main/java/com/alison/commonutils/utils/ClassLoaderUtils.java
+++ src/main/java/com/alison/commonutils/utils/ClassLoaderUtils.java
@@ -7,4 +7,25 @@ package com.alison.commonutils.utils;
  * @Date 2020/11/22 11:09 上午
  **/
 public class ClassLoaderUtils {
+
+
+    private ClassLoaderUtils() {
+    }
+
+
+    public static ClassLoader getSystemClassLoader() {
+        return ClassLoader.getSystemClassLoader();
+    }
+
+
+    public static ClassLoader getClassLoaderFromClazz(Class<?> clazz) {
+        return clazz.getClassLoader();
+    }
+
+
+    public static ClassLoader getClassLoaderFromObject(Object object) {
+        return getClassLoaderFromClazz(object.getClass());
+    }
+
+
 }
