33727a1ea59d9c83ebdcd37852ca9c06295b3ef3
lixianjun02 <lixianjun02@meituan.com>
2020-12-21 21:24:24 +0800

    unit test init

--- src/test/java/com/alison/commonutils/CommonutilsApplicationTests.java
+++ src/test/java/com/alison/commonutils/CommonutilsApplicationTests.java
@@ -1,13 +1,40 @@
 package com.alison.commonutils;
 
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @SpringBootTest
 class CommonutilsApplicationTests {
 
     @Test
     void contextLoads() {
+
+        List<Callable<String>> tasks = Lists.newArrayList();
+        tasks.add(() -> "342");
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<?> future1 = executorService.submit(() -> System.out.println("32423"));
+        Future<String> future2 = executorService.submit(() -> "534523");
+
+        executorService.execute(() -> System.out.println("12345"));
+
+//        Object o = future1.get();
+
+
     }
+
+
+
+
+    private void _______________test_support_____________(){}
+
+
 
 }
